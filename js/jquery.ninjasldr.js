7f608d494806de2e89e7267548c8d4f2bab60d45
juanma.aguero <juanma.aguero@gmail.com>
2014-01-21 16:55:22 -0300

    Asynchronic image behaviour, first commit

--- js/jquery.ninjasldr.js
+++ js/jquery.ninjasldr.js
@@ -11,13 +11,23 @@
 (function($) {
     $.fn.ninjaSlider = function(params) {
         /* default config */
-        var speed = 2000;
+        var speed = 5000;
+        var speedLevel1 = 600;
+        var speedLevel2 = 800;
+        var timeLevel1 = 700;
+        var timeLevel2 = 1500;
         var slideIndex = 0;
+        var slideWidth = 640;
         var slides = new Array();
         var isRunning = false;
+        var isAnimating = false;
         var loopProcess;
-        var sliderSelector = "#" + $(this).attr("id");
+        var mainSelector = "#" + $(this).attr("id");
+        var sliderSelector = mainSelector + " > .ninja-sldr-async-slider";
+        var slideSelector = ".ninja-sldr-slide";
         var loop = false;
+
+
 
 
         /* user config */
@@ -30,23 +40,47 @@
 
 
 
-        slides = $(sliderSelector + " img");
+        slides = $(slideSelector);
 
         var methods = {
-            switchTo: function(data) {
-                for (var index = 0; index < slides.length; index++) {
-                    if ($(slides[index]).attr("src") === data) {
-                        slideIndex = index;
-                        methods.update();
-                        break;
-                    }
-                }
+            switchTo: function(index) {
+                slideIndex = index;
+                methods.update();
             },
             update: function() {
-                $(slides[slideIndex]).stop().fadeIn('slow');
-                for (var i = slideIndex + 1; i < slides.length; i++) {
-                    $(slides[i]).stop().fadeOut('slow');
-                }
+                isAnimating = true;
+                $(sliderSelector).stop().animate({
+                    left: (-1 * slideIndex * slideWidth) + "px"
+                }, 1000, function() {
+                    $(".ninja-sldr-slide-2level").css("right", "-" + slideWidth + "px").hide();
+                    $(".ninja-sldr-slide-1level").css("right", "-100px").hide();
+
+                    /* 1 level */
+                    setTimeout(function() {
+                        var level1 = $(slides[slideIndex]).children(".ninja-sldr-slide-1level");
+                        level1.show();
+                        level1.stop().animate({
+                            right: '15%'
+                        }, speedLevel1, function() {
+                            setTimeout(function() {
+                                $(slides[slideIndex]).children(".ninja-sldr-slide-1level").stop().animate({
+                                    right: '+' + slideWidth
+                                }, speedLevel2, function() {
+                                    /* 2 level */
+                                    var level2 = $(slides[slideIndex]).children(".ninja-sldr-slide-2level");
+                                    level2.show();
+                                    level2.stop().animate({
+                                        right: '-25%'
+                                    }, speedLevel1, function() {
+                                        isAnimating = false;
+                                    });
+                                });
+
+                            }, timeLevel2);
+                        });
+                    }, timeLevel1);
+
+                });
             },
             switchBG: function() {
                 if (slideIndex < slides.length - 1) {
@@ -74,6 +108,11 @@
             stopLoop: function() {
                 clearInterval(loopProcess);
                 isRunning = false;
+            },
+            adjustToWindow: function() {
+                slideWidth = $(mainSelector).width();
+                $(slideSelector).width(slideWidth);
+                $(sliderSelector).width($(slideSelector).length * slideWidth);
             }
         }
 
@@ -82,10 +121,17 @@
                 methods.switchTo($(this).attr("data-ninja"));
             });
         }
+
+        window.onresize = function() {
+            methods.adjustToWindow();
+        };
+
         /* If is set to loop*/
         if (loop) {
             methods.run();
         }
+        methods.update();
+        methods.adjustToWindow();
     }
 
 })(jQuery);
